77659e6f741b43703d8f4c5d7deba8d93b121292
New <aio4t9@protonmail.com>
2026-06-02 13:42:19 +0700

    install

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+
+{!! SEO::generate() !!}
+
+@vite([
+'resources/css/app.css',
+'resources/js/app.js'
+])
+
+</head>
+
+<body>
+
+@yield('content')
+
+</body>
+</html>
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container mx-auto">
+
+    <h1 class="text-4xl font-bold">
+        บ้านมือสองทั่วประเทศ
+    </h1>
+
+</div>
+
+@endsection
